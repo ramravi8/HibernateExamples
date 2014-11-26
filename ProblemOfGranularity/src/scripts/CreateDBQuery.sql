d8d385b261d385aa1240408be75a9c47378ba019
ramravi8 <ramravi8@gmail.com>
2014-11-27 01:25:53 +0530

    committing configuration and mapping files

---
+++ ProblemOfGranularity/src/scripts/CreateDBQuery.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Person
+(
+ id int,
+ name varchar(30),
+ p_street varchar(20),
+ p_city varchar(20),
+ p_state varchar(20),
+ p_country varchar(20),
+ c_street varchar(20),
+ c_city varchar(20),
+ c_state varchar(20),
+ c_country varchar(20)
+)
