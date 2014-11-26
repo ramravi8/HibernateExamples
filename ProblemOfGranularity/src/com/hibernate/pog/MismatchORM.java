927e78974ddba739d4189a0af842dee2a445a3a6
ramravi8 <ramravi8@gmail.com>
2014-11-26 21:27:05 +0530

    Creating Project directory for Hibernate examples - "Mismatch of ORM"

---
+++ ProblemOfGranularity/src/com/hibernate/pog/MismatchORM.java
@@ -0,0 +1,9 @@
+package com.hibernate.pog;
+
+public class MismatchORM 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("This example will solve the mismatch problem of ORM");
+	}
+}
